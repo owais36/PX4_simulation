c2f8dd06f984aaf5945cb58c55044a3c4c210fa0
owais36 <owais.mehmood1991@gmail.com>
2021-05-13 05:43:48 +0200

    bash update

--- setup_sim.bash
+++ setup_sim.bash
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+source /opt/ros/melodic/setup.bash
+catkin build
+source devel/setup.bash
+
 #Check if the PX4 Firmware Folder Exists in user's home directory
 if [ -d /home/$USER/Firmware ]
 then
@@ -24,6 +29,8 @@ echo "Do you want to launch the PX4 simulation:(Y/N)"
 read uservar
 if [ $uservar = "Y" ]
     then
+        cd /home/$USER/Firmware
+        DONT_RUN=1 make px4_sitl_default gazebo
         export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware
         export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware/Tools/sitl_gazebo
         echo ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH
